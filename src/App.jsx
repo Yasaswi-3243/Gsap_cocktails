6b7888f6a74d22b787e0e04b0f9c658d83251bb1
yasaswi-3243 <asrith3243@gmail.com>
2026-02-25 21:47:57 +0530

    implemented the Menu section and made some code changes in the remaining files

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
 import About from './components/About';
 import Art from './components/Art';
+import Menu from './components/menu';
+import Contact from './components/Contact';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
@@ -17,6 +19,8 @@ const App = () => {
             <Cocktails/>
             <About/>  
             <Art/>
+            <Menu/>
+            <Contact/>
         </main>
     )
 }
